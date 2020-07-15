fece4f26d8da85bf8aa7e860460b07d10e64a025
Cs <1453749520@qq.com>
2020-07-15 23:39:32 +0800

    Finish the first requirement

---
+++ src/main/java/com/thoughtworks/basic/Person.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.basic;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public void introduce() {
+        System.out.println("My name is Tom. I am 21 years old.");
+    }
+
+
+}
